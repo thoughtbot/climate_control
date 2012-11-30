ee27d9ae47b2babbaf7aaa437e8029fa3c6823b0
Joshua Clayton <jclayton@thoughtbot.com>
2012-11-30 10:30:41 -0500

    Add spec to ensure block gets run

--- lib/climate_control.rb
+++ lib/climate_control.rb
@@ -3,6 +3,6 @@ require 'climate_control/version'
 
 module ClimateControl
   def self.modify(environment_overrides, &block)
-    Modifier.new(environment_overrides, &block)
+    Modifier.new(environment_overrides, &block).process
   end
 end
--- spec/acceptance/climate_control_spec.rb
+++ spec/acceptance/climate_control_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 describe 'Climate control' do
   it 'allows modification of the environment' do
+    block_run = false
     ClimateControl.modify FOO: 'bar' do
       expect(ENV['FOO']).to eq 'bar'
+      block_run = true
     end
 
     expect(ENV['FOO']).to be_nil
+    expect(block_run).to be_true
   end
 end
