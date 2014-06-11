1dfb22331d3d07e1f4c753afc8960f21e28ecbe6
Ken Dreyer <ktdreyer@ktdreyer.com>
2014-06-11 16:27:30 -0600

    tests: make SimpleCov optional
    Skip loading SimpleCov if it is not present. This allows the tests to
    run outside of Bundler if SimpleCov is not installed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
-require "simplecov"
-SimpleCov.start
+begin
+  require 'simplecov'
+  SimpleCov.start
+rescue LoadError
+  warn "warning: simplecov gem not found; skipping coverage"
+end
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "lib")
 $LOAD_PATH << File.join(File.dirname(__FILE__))
