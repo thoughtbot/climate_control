c7f4611d2a4015264d1062a025e09adc645122b8
Joshua Clayton <jclayton@thoughtbot.com>
2014-11-18 21:17:16 -0500

    Clean up specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,9 @@
-require 'simplecov'
+require "simplecov"
 SimpleCov.start
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "lib")
 $LOAD_PATH << File.join(File.dirname(__FILE__))
 
-require 'rubygems'
-require 'rspec'
-require 'climate_control'
-
-Dir['spec/support/**/*.rb'].each { |f| require File.expand_path(f) }
+require "rubygems"
+require "rspec"
+require "climate_control"
