1a3dc2c7a230309464c30b79ffcebf06626af52e
Joshua Clayton <jclayton@thoughtbot.com>
2014-11-18 21:20:02 -0500

    Update quotes

--- lib/climate_control.rb
+++ lib/climate_control.rb
@@ -1,5 +1,5 @@
-require 'climate_control/modifier'
-require 'climate_control/version'
+require "climate_control/modifier"
+require "climate_control/version"
 
 module ClimateControl
   def self.modify(environment_overrides, &block)
