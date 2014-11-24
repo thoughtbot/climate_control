a6798351a688d339a2c52e1e4a2298acfef055b5
balexand <balexand@gmail.com>
2014-11-24 14:56:35 -0700

    fix thread-unsafe lazy initialization

--- lib/climate_control.rb
+++ lib/climate_control.rb
@@ -4,11 +4,13 @@ require "climate_control/modifier"
 require "climate_control/version"
 
 module ClimateControl
+  @@env = ClimateControl::Environment.new
+
   def self.modify(environment_overrides, &block)
     Modifier.new(env, environment_overrides, &block).process
   end
 
   def self.env
-    @env ||= ClimateControl::Environment.new
+    @@env
   end
 end
