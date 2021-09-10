08b3984d54cee47071bc82a1f7845d4ee255f1fa
Josh Cheek <josh.cheek@gmail.com>
2021-09-10 05:32:52 +0200

    Remove ClimateControl::Modifier
    Just wasn't obvious to me that there was any value in keeping it
    in a separate class, figured I'd try merging it into `ClimateControl`
    itself. In the end, this seemed decent.

--- lib/climate_control.rb
+++ lib/climate_control.rb
@@ -1,17 +1,38 @@
 require "climate_control/errors"
-require "climate_control/modifier"
 require "climate_control/version"
 require "monitor"
 
 module ClimateControl
+  extend self
+
   SEMAPHORE = Monitor.new
   private_constant :SEMAPHORE
 
-  def self.modify(environment_overrides, &block)
-    Modifier.new(SEMAPHORE, env, environment_overrides, &block).call
+  def modify(environment_overrides, &block)
+    environment_overrides = environment_overrides.transform_keys(&:to_s)
+
+    SEMAPHORE.synchronize do
+      pre = mid = env.to_hash
+      copy environment_overrides, into: env
+      mid = env.to_hash
+      block.call
+    ensure
+      (pre.keys | mid.keys | env.keys).each do |key|
+        env[key] = pre[key] if pre[key] != env[key] && mid[key] == env[key]
+      end
+    end
   end
 
-  def self.env
+  def env
     ENV
+  end
+
+  private def copy(overrides, into:)
+    overrides.each do |key, value|
+      into[key] = value
+    rescue TypeError => e
+      raise UnassignableValueError,
+        "attempted to assign #{value} to #{key} but failed (#{e.message})"
+    end
   end
 end
