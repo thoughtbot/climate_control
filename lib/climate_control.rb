d8fd973d8b8db2c8dee308c13937e003f3bb0e69
Dorian Marié <dorian@thoughtbot.com>
2022-05-26 02:39:58 +0200

    Refactor: Use Monitor instead of Mutex
    ### Why was this change necessary?
    
    The code was more complex that it needs to
    
    ### How does it address the problem?
    
    This refactors simplifies the code by using Monitor class
    
    https://docs.ruby-lang.org/en/master/Monitor.html
    
    ### Are there any side effects?
    
    There might be, all the tests pass though
    
    Co-authored-by: Josh Cheek <josh.cheek@gmail.com>

--- lib/climate_control.rb
+++ lib/climate_control.rb
@@ -1,16 +1,44 @@
-require "climate_control/environment"
 require "climate_control/errors"
-require "climate_control/modifier"
 require "climate_control/version"
+require "monitor"
 
 module ClimateControl
-  @@env = ClimateControl::Environment.new
+  extend self
 
-  def self.modify(environment_overrides = {}, &block)
-    Modifier.new(env, environment_overrides, &block).process
+  SEMAPHORE = Monitor.new
+  private_constant :SEMAPHORE
+
+  def modify(environment_overrides = {}, &block)
+    environment_overrides = environment_overrides.transform_keys(&:to_s)
+
+    SEMAPHORE.synchronize do
+      previous = ENV.to_hash
+      middle = {}
+      copy environment_overrides
+      middle = ENV.to_hash
+      block.call
+    ensure
+      after = ENV
+      (previous.keys | middle.keys | after.keys).each do |key|
+        if previous[key] != after[key] && middle[key] == after[key]
+          ENV[key] = previous[key]
+        end
+      end
+    end
   end
 
-  def self.env
-    @@env
+  def env
+    ENV
+  end
+
+  private
+
+  def copy(overrides)
+    overrides.each do |key, value|
+      ENV[key] = value
+    rescue TypeError => e
+      raise UnassignableValueError,
+        "attempted to assign #{value} to #{key} but failed (#{e.message})"
+    end
   end
 end
