d556bf405b2736e22ccfe7acfc36720267bed42d
Dorian Marié <dorian@thoughtbot.com>
2022-07-15 12:35:18 +0200

    Added: `ClimateControl.unsafe_modify` (thread-unsafe `modify`) (#48)
    ### Why was this change necessary?
    
    When using Fiber (like minitest-around does), we can't share a semaphore
    between Fibers (change from Ruby on purpose)
    
    ### How does it address the problem?
    
    `unsafe_modify` is the same as `modify` except that it doesn't get a
    lock on `ENV`
    
    ### Are there any side effects?
    
    `unsafe_modify` doesn't use the semaphore thus will not guarantee
    consistency of ENV if used with threads.

--- lib/climate_control.rb
+++ lib/climate_control.rb
@@ -31,6 +31,27 @@ module ClimateControl
     end
   end
 
+  def unsafe_modify(environment_overrides = {}, &block)
+    environment_overrides = environment_overrides.transform_keys(&:to_s)
+
+    previous = ENV.to_hash
+
+    begin
+      copy environment_overrides
+    ensure
+      middle = ENV.to_hash
+    end
+
+    block.call
+  ensure
+    after = ENV
+    (previous.keys | middle.keys | after.keys).each do |key|
+      if previous[key] != after[key] && middle[key] == after[key]
+        ENV[key] = previous[key]
+      end
+    end
+  end
+
   def env
     ENV
   end
--- lib/climate_control/version.rb
+++ lib/climate_control/version.rb
@@ -1,3 +1,3 @@
 module ClimateControl
-  VERSION = "1.1.1".freeze
+  VERSION = "1.2.0".freeze
 end
